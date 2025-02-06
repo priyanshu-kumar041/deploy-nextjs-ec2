0196e8ebc5d705637f7949c7ccd699db0ac01c62
RAB_5 <priyanshu00268@gmail.com>
2025-02-06 19:29:05 +0530

    just changed to push

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ interface BirthdayProps {
 const BirthdayWishes: React.FC<BirthdayProps> = ({ name }) => {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-yellow-400 text-gray-800 font-sans text-center">
-      <h1 className="text-5xl font-extrabold text-orange-600">Happy Birthday, {name}!</h1>
+      <h1 className="text-5xl font-extrabold text-orange-600">Happy Birthday, {name} Singh!</h1>
       <p className="text-xl mt-4">Wishing you a fantastic year ahead!</p>
     </div>
   );
